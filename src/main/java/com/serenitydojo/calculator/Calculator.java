4aea36f092c96d56d46254789c8ba97f3ee6341f
Carlos Ramirez <32551875+CarlosRamirezGriego@users.noreply.github.com>
2023-12-13 14:31:46 -0700

    Calculator evaluate method implemented
    - Added exception
    - Added code in method

--- src/main/java/com/serenitydojo/calculator/Calculator.java
+++ src/main/java/com/serenitydojo/calculator/Calculator.java
@@ -1,7 +1,53 @@
 package com.serenitydojo.calculator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Calculator {
-    public int evaluate(String expression) {
-        return 0;
+    public int evaluate(String expression) throws IllegalMathsOperatorException {
+        expression = expression.trim().replaceAll("\\s+","");
+        String regex = "^[0-9+\\-*/]+$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(expression);
+        int result = 0;
+
+        if(expression.equals(""))
+        {
+            return 0;
+        }
+        if(matcher.matches())
+        {
+            String[] splittedByCharacter = expression.trim().split("[-+*/]");
+            String[] characters = expression.trim().split("[0-9]+");
+            int numbersAvailable = splittedByCharacter.length;
+            int count = 1;
+            result = Integer.parseInt(splittedByCharacter[0]);
+            while(count <= numbersAvailable-1)
+            {
+                int value = Integer.parseInt(splittedByCharacter[count]);
+                if(characters[count].equals("+"))
+                {
+                    result = result + value;
+                }
+                else if (characters[count].equals("-"))
+                {
+                    result = result - value;
+                }
+                else if (characters[count].equals("*"))
+                {
+                    result = result * value;
+                }
+                else if (characters[count].equals("/"))
+                {
+                    result = result / value;
+                }
+                count++;
+            }
+        }
+        else
+        {
+            throw new IllegalMathsOperatorException("Invalid Operator");
+        }
+        return result;
     }
 }
